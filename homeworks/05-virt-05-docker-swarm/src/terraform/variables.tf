12e632c922adfd43923ebd4671f6c9a9621cc298
Дмитрий Галышев <frozzz34@gmail.com>
2022-09-03 22:08:21 +0700

    05-05 swarm

---
+++ homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gef0jl0aa6n1eiac0n"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gji6q1huce1gbs41u0"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8ud1ik7uoo70q6m19r"
+}
