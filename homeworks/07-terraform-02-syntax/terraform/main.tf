0da8fe6ca3fc61f455c7d3d3f33b37bea9496e5a
frozzy <frozzz34@gmail.com>
2022-11-28 23:14:26 +0700

    complete terraform 07-02

--- homeworks/07-terraform-02-syntax/terraform/main.tf
+++ homeworks/07-terraform-02-syntax/terraform/main.tf
@@ -2,13 +2,21 @@ provider "yandex" {
   service_account_key_file = "key.json"
 }
 
-resource "yandex_compute_instance" "vm" {
-  name = "terraform_vm"
+resource "yandex_compute_instance" "node01" {
+  name = "node01"
+  zone = "ru-central1-a"
 
   resources {
     cores  = 4
     memory = 4
   }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8hbaoni28r5s8vndl9"
+    }
+  }
+
 
   network_interface {
     subnet_id = "${yandex_vpc_subnet.default.id}"
