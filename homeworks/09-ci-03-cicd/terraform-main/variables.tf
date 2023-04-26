bf20b9f709159805770ec47ad6718155e2dffdfc
frozzy <frozzz34@gmail.com>
2023-04-27 00:13:28 +0700

    sonarqube

--- homeworks/09-ci-03-cicd/terraform-main/variables.tf
+++ homeworks/09-ci-03-cicd/terraform-main/variables.tf
@@ -1,11 +1,11 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = ""
+  default = "b1gef0jl0aa6n1eiac0n"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = ""
+  default = "b1g4gs6pra6ndk9uvvc4"
 }
